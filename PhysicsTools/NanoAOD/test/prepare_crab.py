3b8a0f434c09783fb3ac0aca8d3af894ee7a98fa
Yongbin Feng <ybfeng94@gmail.com>
2021-03-04 07:32:22 +0100

    update for running with crab

--- PhysicsTools/NanoAOD/test/prepare_crab.py
+++ PhysicsTools/NanoAOD/test/prepare_crab.py
@@ -6,16 +6,16 @@ import os
 
 samples = [
     # isData, Dataset name, nfiles_per_job
-    (0, '/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 5), # 82 files 
+    (0, '/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 2), # 82 files 
     (0, '/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 2), # 42 files
-    (0, '/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 10), # 169
-    (0, '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 20), # 266
-    (0, '/WJetsToLNu_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 20), # 20
+    (0, '/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 2), # 169
+    (0, '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 2), # 266
+    (0, '/WJetsToLNu_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 2), # 395
     (0, '/WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2-v1/MINIAODSIM', 2), # 26
     (0, '/WWTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 2), # 6
     (0, '/WZTo3LNu_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-fixECALGT_LowPU_94X_mc2017_realistic_v10For2017H_v2_ext1-v1/MINIAODSIM', 2), # 7
-    (1, '/HighEGJet/Run2017H-17Nov2017-v1/MINIAOD', 20), # 1070 files in total
-    (1, '/SingleMuon/Run2017H-17Nov2017-v2/MINIAOD', 20), # 428 files in total
+    (1, '/HighEGJet/Run2017H-17Nov2017-v1/MINIAOD', 10), # 1070 files in total
+    (1, '/SingleMuon/Run2017H-17Nov2017-v2/MINIAOD', 10), # 428 files in total
 ]
 
 config_dir = 'crab_configs'
@@ -44,7 +44,7 @@ for isData, path, nfiles_per_job in samples:
 
     file_entries.append('')
     file_entries.append('config.section_("General")')
-    file_entries.append('config.General.requestName = "production_LowPUNano_%s"' %(base_name))
+    file_entries.append('config.General.requestName = "production_LowPUNano_v2_%s"' %(base_name))
     file_entries.append('config.General.workArea = "crab_projects"')
     file_entries.append('config.General.transferLogs = False')
     file_entries.append('config.General.transferOutputs = True')
@@ -54,7 +54,7 @@ for isData, path, nfiles_per_job in samples:
     file_entries.append('config.JobType.pluginName = "Analysis"')
     file_entries.append('config.JobType.psetName = "nano_Run2017.py"')
     file_entries.append('config.JobType.pyCfgParams = %s'%pyCfgParams)
-    #file_entries.append('config.JobType.maxMemoryMB = 4000 # Default is 2500 : Max I have used is 13000')
+    file_entries.append('config.JobType.maxMemoryMB = 4000 # Default is 2500 : Max I have used is 13000')
     file_entries.append('config.JobType.numCores = 8')
     file_entries.append('config.JobType.allowUndistributedCMSSW = True')
 
@@ -68,6 +68,10 @@ for isData, path, nfiles_per_job in samples:
     file_entries.append('config.Data.publication = False')
     file_entries.append('config.Data.outputDatasetTag= "NanoAOD_0302"')
     file_entries.append('config.Data.outLFNDirBase = "/store/user/yofeng/LowPU/"')
+
+    file_entries.append('')
+    file_entries.append('config.section_("Site")')
+    file_entries.append('config.Site.blacklist = ["T2_US_Caltech"]')
 
     if isData:
         file_entries.append('config.Data.lumiMask = "/afs/cern.ch/work/y/yofeng/public/WpT/CMSSW_10_6_20/src/PhysicsTools/NanoAOD/test/Cert_306896-307082_13TeV_PromptReco_Collisions17_JSON_LowPU_lowPU.txt"')
